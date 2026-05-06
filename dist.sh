017400c9bf6e5a499cdf61f506673f838aa2240b
Matthew Molloy <whamtet@gmail.com>
2026-05-06 22:34:08 +0700

    just one outstanding bug

---
+++ dist.sh
@@ -0,0 +1,55 @@
+d=dist
+mkdir $d
+cp index.html $d
+cp reply.html $d
+
+mkdir -p $d/assets/images/logo
+mkdir -p $d/assets/images/hero
+mkdir -p $d/assets/images/about
+mkdir -p $d/assets/images/testimonials
+mkdir -p $d/assets/images/team
+mkdir -p $d/assets/images/brands
+mkdir -p $d/assets/images/blog
+mkdir -p $d/assets/images/footer
+mkdir -p $d/assets/css
+mkdir -p $d/assets/js
+mkdir -p $d/src/css
+
+cp assets/images/logo/logo-white.svg $d/assets/images/logo
+cp assets/images/hero/hero-image.png $d/assets/images/hero
+cp assets/images/about/about-image-01.png $d/assets/images/about
+cp assets/images/about/about-image-02.png $d/assets/images/about
+cp assets/images/testimonials/icon-star.svg $d/assets/images/testimonials
+cp assets/images/testimonials/author-01.jpg $d/assets/images/testimonials
+cp assets/images/testimonials/author-02.jpg $d/assets/images/testimonials
+cp assets/images/testimonials/author-03.jpg $d/assets/images/testimonials
+cp assets/images/team/team-03.jpeg $d/assets/images/team
+cp assets/images/team/team-02.jpeg $d/assets/images/team
+cp assets/images/brands/graygrids.svg $d/assets/images/brands
+cp assets/images/brands/graygrids-white.svg $d/assets/images/brands
+cp assets/images/brands/lineicons.svg $d/assets/images/brands
+cp assets/images/brands/lineicons-white.svg $d/assets/images/brands
+cp assets/images/brands/uideck.svg $d/assets/images/brands
+cp assets/images/brands/uideck-white.svg $d/assets/images/brands
+cp assets/images/brands/ayroui.svg $d/assets/images/brands
+cp assets/images/brands/ayroui-white.svg $d/assets/images/brands
+cp assets/images/brands/tailgrids.svg $d/assets/images/brands
+cp assets/images/brands/tailgrids-white.svg $d/assets/images/brands
+cp assets/images/blog/blog-footer-01.jpg $d/assets/images/blog
+cp assets/images/blog/blog-footer-02.jpg $d/assets/images/blog
+cp assets/images/footer/shape-1.svg $d/assets/images/footer
+cp assets/images/footer/shape-3.svg $d/assets/images/fooer
+cp assets/images/logo/logo-white.svg $d/assets/images/logo
+
+cp assets/css/swiper-bundle.min.css $d/assets/css/swiper-bundle.min.css
+cp assets/css/animate.css $d/assets/css/swiper-bundle.min.css
+mv $d/output.css $d/src/css/tailwind.css
+
+cp assets/js/wow.min.js $d/assets/js/wow.min.js
+
+cp assets/js/swiper-bundle.min.js $d/assets/js/swiper-bundle.min.js
+cp assets/js/main.js $d/assets/js/main.js
+cp assets/images/footer/shape-3.svg $d/assets/images/footer/shape-3.svg
+
+cp assets/css/animate.css $d/assets/css/animate.css
+cp assets/images/logo/logo.svg $d/assets/images/logo
